f884b90ae7204260955bb8f1c89f503dec0df7ca
Luis Amez <luis.amez.vitoria@gmail.com>
2017-09-26 17:45:04 +0200

    Fix bug with price-display template

--- src/app/price-display/price-display.component.ts
+++ src/app/price-display/price-display.component.ts
@@ -2,7 +2,7 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'price-display',
-  template: `<div class="price-display>\${{ price }}</div>`,
+  template: `<div class="price-display">\${{ price }}</div>`,
   styleUrls: ['./price-display.component.css']
 })
 export class PriceDisplayComponent {
